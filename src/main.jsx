a846aa562c1d6c35bad570d3e3920b65bebe4d3e
mazinramos <mazenramos0@gmail.com>
2026-03-28 20:23:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' 
 
-// هذا الملف هو الجسر بين ملف الـ HTML وكود الـ App.jsx المطور
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
